901b90142ad1a517d0a375b7af4ee75ad2758ec8
DsunilK <sunilkumardevalla@gmail.com>
2021-03-05 18:58:59 -0500

    Add token

--- bin/bot.rb
+++ bin/bot.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'telegram_bot'
 token = '1631929196:AAFj5IPu_d83nMnodtwiaq_thFgip854QaU'
 bot = TelegramBot.new(token: token)
@@ -9,9 +11,9 @@ bot.get_updates(fail_silently: true) do |message|
   message.reply do |reply|
     case command
     when /start/i
-      reply.text = "All I can do is say hello. Try the /greet command."
+      reply.text = 'All I can do is say hello. Try the /greet command.'
     when /greet/i
-      greetings = ['bonjour', 'hola', 'hallo', 'sveiki', 'namaste', 'salaam', 'szia', 'halo', 'ciao']
+      greetings = %w[bonjour hola hallo sveiki namaste salaam szia halo ciao]
       reply.text = "#{greetings.sample.capitalize}, #{message.from.first_name}!"
     else
       reply.text = "I have no idea what #{command.inspect} means."
