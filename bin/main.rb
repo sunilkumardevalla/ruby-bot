9e37c19941998bdb1fa7ff22d1e6c204ef313599
DsunilK <sunilkumardevalla@gmail.com>
2021-03-09 11:48:18 -0500

    Add puts in main file

--- bin/main.rb
+++ bin/main.rb
@@ -2,8 +2,8 @@
 
 require_relative '../lib/bot.rb'
 
-puts 'Hello there!!'
+puts 'Hello there!'
 puts 'Nice to meet you'
-puts "I'm Chocsun bot, and I'm going to help you play random music for you, the choice is up to you."
+puts "I'm Chocsun bot, and I'm going to help you play random music for you, Lets go..."
 
 Bot.new
