49d925c1c2440481e5694e6cbf85003be1f5c620
DsunilK <sunilkumardevalla@gmail.com>
2021-03-09 11:53:31 -0500

    Add music in lib

--- lib/music.rb
+++ lib/music.rb
@@ -2,17 +2,17 @@
 require_relative 'bot.rb'
 
 class Music
-  attr_reader :rock, :techno, :hiphop, :romantic
+  attr_reader :romantic, :techno, :rock, :hiphop 
 
   def initialize
-    @rock = [
-      'https://www.youtube.com/watch?v=zUzd9KyIDrM',
-      'https://www.youtube.com/watch?v=vjVkXlxsO8Q',
-      'https://www.youtube.com/watch?v=Aiay8I5IPB8',
-      'https://www.youtube.com/watch?v=p0vM9iINl28',
-      'https://www.youtube.com/watch?v=Sb5aq5HcS1A',
-      'https://www.youtube.com/watch?v=Q0oIoR9mLwc',
-      'https://www.youtube.com/watch?v=eVTXPUF4Oz4'
+    @romantic = [
+      'https://www.youtube.com/watch?v=lp-EO5I60KA&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy',
+      'https://www.youtube.com/watch?v=2Vv-BfVoq4g&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=2',
+      'https://www.youtube.com/watch?v=KRMOPcaIfuI&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=3',
+      'https://www.youtube.com/watch?v=450p7goxZqg&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=4',
+      'https://www.youtube.com/watch?v=0yW7w8F2TVA&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=5',
+      'https://www.youtube.com/watch?v=Pkh8UtuejGw&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=6',
+      'https://www.youtube.com/watch?v=SnAOVJeg-yE&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=7'
     ]
 
     @techno = [
@@ -25,6 +25,16 @@ class Music
       'https://www.youtube.com/watch?v=jB-zsM6aPPo&list=RDCLAK5uy_kLWIr9gv1XLlPbaDS965-Db4TrBoUTxQ8&index=3'
     ]
 
+    @rock = [
+      'https://www.youtube.com/watch?v=zUzd9KyIDrM',
+      'https://www.youtube.com/watch?v=vjVkXlxsO8Q',
+      'https://www.youtube.com/watch?v=Aiay8I5IPB8',
+      'https://www.youtube.com/watch?v=p0vM9iINl28',
+      'https://www.youtube.com/watch?v=Sb5aq5HcS1A',
+      'https://www.youtube.com/watch?v=Q0oIoR9mLwc',
+      'https://www.youtube.com/watch?v=eVTXPUF4Oz4'
+    ]
+
     @hiphop = [
       'https://www.youtube.com/watch?v=8AshnB1vq9c',
       'https://www.youtube.com/watch?v=l0U7SxXHkPY',
@@ -34,16 +44,6 @@ class Music
       'https://www.youtube.com/watch?v=ArYlvBGI5Vk&list=RDCLAK5uy_mVRuj5egfh21e-pXyA3ymx_0p4Xlg-c0I&index=3',
       'https://www.youtube.com/watch?v=H2Po5uiZMco&list=RDCLAK5uy_mVRuj5egfh21e-pXyA3ymx_0p4Xlg-c0I&index=4'
     ]
-
-    @romantic = [
-      'https://www.youtube.com/watch?v=lp-EO5I60KA&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy',
-      'https://www.youtube.com/watch?v=2Vv-BfVoq4g&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=2',
-      'https://www.youtube.com/watch?v=KRMOPcaIfuI&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=3',
-      'https://www.youtube.com/watch?v=450p7goxZqg&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=4',
-      'https://www.youtube.com/watch?v=0yW7w8F2TVA&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=5',
-      'https://www.youtube.com/watch?v=Pkh8UtuejGw&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=6',
-      'https://www.youtube.com/watch?v=SnAOVJeg-yE&list=PL64G6j8ePNureM8YCKy5nRFyzYf8I2noy&index=7'
-    ]
   end
 
   def music_select(category)
@@ -52,9 +52,9 @@ class Music
 
   def sentences
     {
-      'start_message' => "Hello I'm Chocsun bot that going to give you soulfull music. Type /help to see the commands",
-      'commands' => "Here are the commands: \n /start \n /stop \n /music \n /help",
-      'wrong_entry' => "Wrong entry, please entry one of this options:/start \n /music \n /stop \n /help",
+      'start_message' => "Hello I'm Chocsun bot that going to give you soulfull music. Type /help to go",
+      'commands' => "Here is how you can do: \n /start \n /stop \n /music \n /help",
+      'wrong_entry' => "Wrong entry, please enter from above available options:/start \n /music \n /stop \n /help",
       'stop_message' => 'Sorry to see you go, See you again',
     }
   end
